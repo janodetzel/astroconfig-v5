00045056f867c73abd0244617d3fac0302c0f175
Jano Detzel <jano.detzel@gmail.com>
2025-12-15 11:57:03 +0100

    Use prettier instead of prettierd for formatting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -20,7 +20,7 @@ return {
       -- Set a formatter
       null_ls.builtins.formatting.stylua,
 
-      null_ls.builtins.formatting.prettierd,
+      null_ls.builtins.formatting.prettier,
       null_ls.builtins.code_actions.refactoring,
     })
   end,
